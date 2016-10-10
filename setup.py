7bf767b390e67d704202c3173649b6e9dbc62a74
Leon Jacobs <leonja511@gmail.com>
2016-10-10 12:26:09 +0200

    Release v1.1

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     author='Leon Jacobs',
     author_email='leonja511@gmail.com',
     url='https://github.com/leonjza/ooktools',
-    download_url='https://github.com/leonjza/ooktools/tarball/1.0',
+    download_url='https://github.com/leonjza/ooktools/tarball/1.1',
     keywords=['sdr', 'on-off', 'keying', 'rfcat', 'radio'],
-    version='1.0',
+    version='1.1',
     packages=[
         'ooktools',
         'ooktools.commands',
